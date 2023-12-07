b88907fcbbcc8c82d28548db4d942b4e25f75e8a
Catalina <202104665@alu.comillas.edu>
2023-12-07 23:13:40 +0100

    Arreglos finales

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.Port;
 
 public class AI : MonoBehaviour
 {
